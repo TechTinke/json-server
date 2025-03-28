15f5f32089a93da657dfa59e35e0b8426063bae2
Oscar Somba <maingioscar2@gmail.com>
2025-03-29 01:51:30 +0300

    final changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults();
+const middlewares = jsonServer.defaults({ static: "./public" }); // Point to your public directory
 
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
